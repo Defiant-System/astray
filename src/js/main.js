2b90aec5102bfb36a1515445c06227a11039ff4f
Hakan Bilgin <hbi99@hotmail.com>
2021-12-03 18:27:54 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.set(1, 1, 5);
 
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.15);
-// scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientLight);
 
 const pointLight = new THREE.PointLight(0xffffff, 1);
 pointLight.position.set(1, 1, 1.3);
@@ -26,12 +26,32 @@ let loader = new THREE.TextureLoader(),
 		plane: { path: "~/img/concrete.png" },
 		ball:  { path: "~/img/ball.png" },
 	},
+	keyAxis = [0, 0],
 	ballMesh,
 	ballRadius = 0.25,
 	mazeDimension = 11,
 	mazeMesh,
-	maze;
+	maze,
+	// Box2D shortcuts
+	b2World        = Box2D.Dynamics.b2World,
+	b2FixtureDef   = Box2D.Dynamics.b2FixtureDef,
+	b2BodyDef      = Box2D.Dynamics.b2BodyDef,
+	b2Body		   = Box2D.Dynamics.b2Body,
+	b2CircleShape  = Box2D.Collision.Shapes.b2CircleShape,
+	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+	b2Settings     = Box2D.Common.b2Settings,
+	b2Vec2         = Box2D.Common.Math.b2Vec2,
+	wWorld,
+	wBall;
 
+var bounciness = 0.9;
+var gravity = 0.2;
+var velocityX = 1; //Math.cos(angleRad) * power;
+var velocityY = 1; //Math.sin(angleRad) * power;
+var velocityZ = 1;
+var ballCircumference = Math.PI * ballRadius * 2;
+var ballVelocity = new THREE.Vector3();
+var ballRotationAxis = new THREE.Vector3();
 
 
 const astray = {
@@ -56,6 +76,12 @@ const astray = {
 			case "window.open":
 				break;
 			case "window.keystroke":
+				switch (event.keyCode) {
+					case 37: keyAxis = [-1, 0]; break;  // left
+					case 39: keyAxis = [1,  0]; break;  // right
+					case 38: keyAxis = [0,  1]; break;  // up
+					case 40: keyAxis = [0, -1]; break;  // down
+				}
 				break;
 			case "open-help":
 				defiant.shell("fs -u '~/help/index.md'");
@@ -65,28 +91,62 @@ const astray = {
 				maze = Maze.generate(mazeDimension);
 				maze[mazeDimension-1][mazeDimension-2] = false;
 
-				// Add the maze
-				mazeMesh = Self.generateMazeMesh(maze);
-				scene.add(mazeMesh);
-
-				// Add ball
-				let ballGeo = new THREE.SphereGeometry(ballRadius, 32, 16);
-				let ballMat = new THREE.MeshStandardMaterial({ map: textures.ball.map });
-				ballMesh = new THREE.Mesh(ballGeo, ballMat);
-				ballMesh.position.set(1, 1, ballRadius);
-				scene.add(ballMesh);
-
-				// Add the ground
-				let planeGeo = new THREE.PlaneGeometry(mazeDimension + 2, mazeDimension + 2, mazeDimension, mazeDimension);
-				let planeMat = new THREE.MeshStandardMaterial({ color: 0x555555 });
-				let planeMesh = new THREE.Mesh (planeGeo, planeMat);
-				planeMesh.position.set((mazeDimension-1)/2, (mazeDimension-1)/2, 0);
-				scene.add(planeMesh);
+				Self.createPhysicsWorld();
+				Self.createRenderWorld();
 
 				// start loop
 				Self.animate();
 				break;
 		}
+	},
+	createPhysicsWorld() {
+		// Create the world object.
+		wWorld = new b2World(new b2Vec2(0, 0), true);
+
+		// Create the ball.
+		var bodyDef = new b2BodyDef();
+		bodyDef.type = b2Body.b2_dynamicBody;
+		bodyDef.position.Set(1, 1);
+		wBall = wWorld.CreateBody(bodyDef);
+		var fixDef = new b2FixtureDef();
+		fixDef.density = 1.0;
+		fixDef.friction = 0.0;
+		fixDef.restitution = 0.25;
+		fixDef.shape = new b2CircleShape(ballRadius);
+		wBall.CreateFixture(fixDef);
+
+		// Create the maze.
+		bodyDef.type = b2Body.b2_staticBody;
+		fixDef.shape = new b2PolygonShape();
+		fixDef.shape.SetAsBox(0.5, 0.5);
+		for (var i = 0; i < maze.dimension; i++) {
+			for (var j = 0; j < maze.dimension; j++) {
+				if (maze[i][j]) {
+					bodyDef.position.x = i;
+					bodyDef.position.y = j;
+					wWorld.CreateBody(bodyDef).CreateFixture(fixDef);
+				}
+			}
+		}
+	},
+	createRenderWorld() {
+		// Add the maze
+		mazeMesh = this.generateMazeMesh(maze);
+		scene.add(mazeMesh);
+
+		// Add ball
+		let ballGeo = new THREE.SphereGeometry(ballRadius, 32, 16);
+		let ballMat = new THREE.MeshStandardMaterial({ map: textures.ball.map });
+		ballMesh = new THREE.Mesh(ballGeo, ballMat);
+		ballMesh.position.set(1, 1, ballRadius);
+		scene.add(ballMesh);
+
+		// Add the ground
+		let planeGeo = new THREE.PlaneGeometry(mazeDimension + 2, mazeDimension + 2, mazeDimension, mazeDimension);
+		let planeMat = new THREE.MeshStandardMaterial({ color: 0x555555 });
+		let planeMesh = new THREE.Mesh (planeGeo, planeMat);
+		planeMesh.position.set((mazeDimension-1)/2, (mazeDimension-1)/2, 0);
+		scene.add(planeMesh);
 	},
 	generateMazeMesh(field) {
 		let totalGeom = new THREE.Geometry(),
@@ -106,11 +166,60 @@ const astray = {
 		}
 		return new THREE.Mesh(totalGeom, material);
 	},
+	updatePhysicsWorld() {
+		// Apply "friction". 
+		var velocity = wBall.GetLinearVelocity();
+		velocity.Multiply(0.95);
+		wBall.SetLinearVelocity(velocity);
+		
+		// Apply user-directed force.
+		var force = new b2Vec2(keyAxis[0] * wBall.GetMass() * 0.95, keyAxis[1] * wBall.GetMass() * 0.95);
+		wBall.ApplyImpulse(force, wBall.GetPosition());
+		keyAxis = [0, 0];
+
+		// Take a time step.
+		wWorld.Step(1/60, 8, 3);
+	},
+	updateRenderWorld() {
+		// Update ball position.
+		var stepX = wBall.GetPosition().x - ballMesh.position.x;
+		var stepY = wBall.GetPosition().y - ballMesh.position.y;
+
+		ballMesh.position.x += stepX;
+		ballMesh.position.y += stepY;
+		ballMesh.position.z += velocityZ;
+
+		if (ballMesh.position.z < ballRadius) {
+			velocityZ *= -bounciness;
+			ballMesh.position.z = ballRadius;
+		}
+
+		ballVelocity.set(stepX, stepY, velocityZ);
+		ballRotationAxis.set(0, 0, ballRadius).cross(ballVelocity).normalize();
+
+		let velocityMag = ballVelocity.length();
+		let rotationAmount = velocityMag * (Math.PI * 2) / ballCircumference;
+		ballMesh.rotateOnWorldAxis(ballRotationAxis, rotationAmount);
+		
+		velocityZ -= gravity;
+
+		// Update camera and light positions.
+		camera.position.x += (ballMesh.position.x - camera.position.x) * 0.1;
+		camera.position.y += (ballMesh.position.y - camera.position.y) * 0.1;
+		camera.position.z += (5 - camera.position.z) * 0.1;
+		pointLight.position.x = camera.position.x;
+		pointLight.position.y = camera.position.y;
+		pointLight.position.z = camera.position.z - 3.7;
+	},
 	animate() {
-		requestAnimationFrame(astray.animate);
-		// cube.rotation.x += 0.04;
-		// cube.rotation.y += 0.04;
+		let Self = astray;
+
+		Self.updatePhysicsWorld();
+		Self.updateRenderWorld();
+
 		renderer.render(scene, camera);
+
+		requestAnimationFrame(astray.animate);
 	}
 };
 
